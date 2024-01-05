c977a2a3d568ed610360e28dc80c0553145f91d1
emmachancellor <ecdyer@sbcglobal.net>
2024-01-05 16:02:07 -0600

    Create segment_image()

--- attiicc/segmentation/sam.py
+++ attiicc/segmentation/sam.py
@@ -31,4 +31,24 @@ def load_sam_model(model_type: str) -> sa.Sam:
     print("Model Loaded")
     return sam
 
-load_sam_model('vit_h')
+def segment_image(sam: sa.Sam, img_dir_path: str):
+    '''
+    Segments an image using a pretrained SAM model.
+    Inputs:
+        sam (Sam): The pretrained SAM model.
+        img_dir_path (str): The path to the image to segment.
+    Outputs:
+        sam_result (dict): The segmentation results. Contains: 
+            `segmentation` : the mask 
+            `area` : the area of the mask in pixels
+            `bbox` : the boundary box of the mask in XYWH format
+            `predicted_iou` : the model's own prediction for the quality of the mask
+            `point_coords` : the sampled input point that generated this mask
+            `stability_score` : an additional measure of mask quality
+            `crop_box` : the crop of the image used to generate this mask in XYWH format
+    '''
+    mask_generator = sa.SamAutomaticMaskGenerator(sam)
+    image_bgr = cv2.imread(img_dir_path) # cv2 reads in BGR format
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB) # convert to RGB
+    sam_result = mask_generator.generate(image_rgb)
+    return sam_result
