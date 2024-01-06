aa12b99142ef840dcba23d0c62d22094215b8c09
emmachancellor <ecdyer@sbcglobal.net>
2024-01-05 19:15:22 -0600

    Testing sam.py

--- attiicc/segmentation/sam.py
+++ attiicc/segmentation/sam.py
@@ -3,6 +3,7 @@ import torch
 import cv2
 import os
 import supervision as sv
+import pkg_resources
 from attiicc import segment_anything as sa
 
 class SamSegmenter:
@@ -13,20 +14,20 @@ class SamSegmenter:
 
     def __init__( 
         self,
-        model_type: str = "vit-h",
+        model_type: str = "vit_h",
         image_path: str = None,
     ) -> None:
         '''
         Initialize a SAM model and calculate the segmentation for an image.
         Inputs:
             model_type (str, optional): Specify the sam model type to load.
-            Default is "vit-h". Can use "vit-b", "vit-l", or "vit-h".
+            Default is "vit_h". Can use "vit_b", "vit_l", or "vit_h".
             image_path (str, optional): The path to the image to segment. 
         Outputs:
             None
         '''
         self.model_type = model_type
-        self.sam = self.load_sam_model(self.model_type)
+        self.sam = self._load_sam_model(self.model_type)
         self.sam_result = None
         self.segmentation = None
         self.area = None
@@ -38,7 +39,7 @@ class SamSegmenter:
 
         if image_path is not None:
             self.image_path = image_path
-            self.sam_result = self.segment_image(self.sam, self.image_path)
+            self.sam_result = self._segment_image(self.sam, self.image_path)
         if self.sam_result is not None:
             self.segmentation = [mask["segmentation"] for mask in self.sam_result]
             self.area = [mask["area"] for mask in self.sam_result]
@@ -48,12 +49,12 @@ class SamSegmenter:
             self.stability_score = [mask["stability_score"] for mask in self.sam_result]
             self.crop_box = [mask["crop_box"] for mask in self.sam_result]
 
-    def _load_sam_model(self) -> sa.Sam:
+    def _load_sam_model(self, model_type) -> sa.Sam:
         '''
         Loads a pretrained SAM model.
         Input:
             model_type (str): Specify the sam model type to load.
-            Can use "vit-b", "vit-l", or "vit-h". 
+            Can use "vit_b", "vit_l", or "vit_h". 
         Output:
             Sam: The loaded SAM model.
         '''
@@ -66,17 +67,24 @@ class SamSegmenter:
         os.environ["CUDA_VISIBLE_DEVICES"] = "0"
         DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         # Path to the checkpoint file
-        HOME = os.getcwd()
-        CHECKPOINT_PATH = os.path.join(HOME, "weights", "sam_vit_h_4b8939.pth")
-        MODEL_TYPE = self.model_type
+        if model_type == "vit_h":
+            CHECKPOINT_PATH = pkg_resources.resource_filename('attiicc', 'segment_anything/weights/sam_vit_h_4b8939.pth')
+        elif model_type == "vit_b":
+            CHECKPOINT_PATH = pkg_resources.resource_filename('attiicc', 'segment_anything/weights/sam_vit_b_4b8939.pth')
+        elif model_type == "vit_l":
+            CHECKPOINT_PATH = pkg_resources.resource_filename('attiicc', 'segment_anything/weights/sam_vit_l_4b8939.pth')
+        MODEL_TYPE = model_type
         # Load the model
         sam = sa.sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH).to(device=DEVICE)
         print("Model Loaded")
         return sam
 
-    def _segment_image(self):
+    def _segment_image(self, sam, image_path):
         '''
         Segments an image using a pretrained SAM model.
+        Inputs:
+            sam (Sam): The loaded SAM model.
+            image_path (str): The path to the image to segment.
         Outputs:
             sam_result (dict): The segmentation results. Contains: 
                 `segmentation` : the mask 
@@ -87,8 +95,8 @@ class SamSegmenter:
                 `stability_score` : an additional measure of mask quality
                 `crop_box` : the crop of the image used to generate this mask in XYWH format
         '''
-        mask_generator = sa.SamAutomaticMaskGenerator(self.sam)
-        image_bgr = cv2.imread(self.image_path) # cv2 reads in BGR format
+        mask_generator = sa.SamAutomaticMaskGenerator(sam)
+        image_bgr = cv2.imread(image_path) # cv2 reads in BGR format
         image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB) # convert to RGB
         sam_result = mask_generator.generate(image_rgb)
         return sam_result
