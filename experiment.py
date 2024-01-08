a27710de99bac605c696031d16059c8923ce18b6
Emma Dyer <ecdyer@sbcglobal.net>
2024-01-08 12:26:10 -0600

    Begin Experiment class __init__

---
+++ experiment.py
@@ -0,0 +1,69 @@
+import numpy as np
+import attiicc as ac
+
+class NanoExperiment:
+    '''Apply segmentation functions to an experiment with multiple channels, 
+    fields of view, and time points.
+
+    This interface is designed to be used with many directories of
+    nanowell images, where the end goal is to create directories
+    for each individual well containing images from every time point collected. 
+    
+    The final number of segmented images equates to:
+    (number of channels) x (number of fields of view) x (number of wells per field) x (number of time points)
+    '''
+
+    def __init__(
+            self,
+            experiment_path: str = None,
+            field_id: str = None,
+            num_fields: int = None,
+            channel_id: str = None,
+            num_channels: int = None,
+            time_point_id: str = None,
+            num_time_points: int = None
+    ) -> None:
+        '''Initialize a NanoExperiment object.
+        The experimental data should be organized as a directory tree with the following structure:
+        experiment_path/
+            field_id_0_channel_id_0/
+                image_time_point_id_0.tif
+                image_time_point_id_1.tif
+                ...
+            field_id_0_channel_id_1/
+                image_time_point_id_0.tif
+                image_time_point_id_1.tif
+                ...
+            ...
+        The experiment directory should have subdirectories that are named according
+        to the field of view and channel identifiers. Each subdirectory should contain
+        the raw .TIF images, with the time point identifier in the file name.
+        
+        Inputs:
+            experiment_path: (str) The path to the experiment parent directory.
+            field_id: (str) The field of view identifier used in direcotry names.
+            num_fields: (int) The number of fields of view in the experiment.
+            channel_id: (str) The channel identifier used in directory names.
+            num_channels: (int) The number of channels in the experiment.
+            time_point_id: (str) The time point identifier used in file names.
+            num_time_points: (int) The number of time points in the experiment.
+        Outputs:
+            None
+        '''
+        assert isinstance(experiment_path, str), 'Experiment_path must be a string'
+        assert isinstance(field_id, str), 'field_id must be a string specifying the field of view identifier, Ex. \'f\''
+        assert isinstance(num_fields, int), 'num_fields must be an integer specifying the number of fields of view'
+        assert isinstance(channel_id, str), 'channel_id must be a string specifying the channel identifier, Ex. \'c\''
+        assert isinstance(num_channels, int), 'num_channels must be an integer specifying the number of channels'
+        assert isinstance(time_point_id, str), 'time_point_id must be a string specifying the time point identifier, Ex. \'t\''
+        assert isinstance(num_time_points, int), 'num_time_points must be an integer specifying the number of time points'
+        self._experiment_path = experiment_path
+        self._field_id = field_id
+        self._num_fields = num_fields
+        self._channel_id = channel_id
+        self._num_channels = num_channels
+        self._time_point_id = time_point_id
+        self._num_time_points = num_time_points
+        self._structure = print(f'Experiment Structure /n field_id: {self.field_id} /n field_num: {self.num_fields} /n \ 
+                               channel_id: {self.channel_id} /n channel_num: {self.num_channels} /n \ 
+                               time_point_id: {self.time_point_id} /n time_point_num: {self.num_time_points}')
